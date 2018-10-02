59d3f969efd47a0232692791929332b0b988d4bc
imamachi-n <imamachigatugatu@hotmail.co.jp>
2018-10-02 20:18:05 +0900

    add GET method in search page.

--- src/main/java/com/rnaomix/itemmanagement/ItemManagementApplication.java
+++ src/main/java/com/rnaomix/itemmanagement/ItemManagementApplication.java
@@ -53,6 +53,19 @@ public class ItemManagementApplication {
             itemRepository.save(item2);
             itemRepository.save(item3);
 
+            Item item4 = new Item("S004", "リケン ふえるわかめちゃん国内 16g", 1000L);
+            Item item5 = new Item("S005", "ケロッグ コーンフロスティ 袋 240g", 1000L);
+            Item item6 = new Item("S006", "江崎グリコ トマトプリッツ<9袋> 134g", 1000L);
+            Item item7 = new Item("S007", "クイックルワイパー フロア用掃除道具 ハンディ 取替用", 1000L);
+            Item item8 = new Item("S008", "キッチンクイックル つめかえ用 ジャンボパック 24枚入", 1000L);
+            Item item9 = new Item("S009", "エリエール ティッシュ キュート 160組×5箱 パルプ", 1000L);
+            itemRepository.save(item4);
+            itemRepository.save(item5);
+            itemRepository.save(item6);
+            itemRepository.save(item7);
+            itemRepository.save(item8);
+            itemRepository.save(item9);
+
             // Add itemHistory and save these to DB
             ItemHistoryDetail itemHistoryDetail1 = new ItemHistoryDetail();
             ItemHistoryDetail itemHistoryDetail2 = new ItemHistoryDetail();
--- src/main/java/com/rnaomix/itemmanagement/controller/SearchController.java
+++ src/main/java/com/rnaomix/itemmanagement/controller/SearchController.java
@@ -1,5 +1,7 @@
 package com.rnaomix.itemmanagement.controller;
 
+import com.rnaomix.itemmanagement.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/search")
 public class SearchController {
 
+    private ItemService itemService;
+
+    @Autowired
+    public SearchController(ItemService itemService){
+        this.itemService = itemService;
+    }
+
     @GetMapping("/list")
     public String getSearchList(Model model){
+
+        model.addAttribute("items", itemService.getItemList());
         return "/search/list";
     }
 }
